bf3fb4327db78d12066cb3cf131b7b954d689413
tanjil <tanjil_ahmed@hotmail.com>
2018-05-11 20:25:28 -0400

    Final Version:Implementation of the Algorithm RaTa

--- SeqCluster.py
+++ SeqCluster.py
@@ -1,9 +1,19 @@
-#Final Version: gene weight corrected; date: 5/10/18 10:09 PM
+#!/usr/bin/python
+#title           :SeqCluster.py
+#description     :Implementation of Algorithm RaTa
+#author          :Tanjil Ahmed
+#date            :05/11/2018
+#version         :1.0
+#usage           :python SeqCluster.py <filename.fasta>
+#==============================================================================
+
 import sys
 import numpy as np
 import math
 import pandas as pd
 
+#################################BEGIN##########################################
+#check if the input DNA sequences contain characters from {A,C,T,G,R,Y,M}
 def file_format_check(fasta):
         string="ACTGRYM"
         for line_number,line in enumerate(fasta):
@@ -12,7 +22,10 @@ def file_format_check(fasta):
                 if any(c not in string for c in sequence):
                     return 0
         return 1
+#################################END############################################
 
+#################################BEGIN##########################################
+#calculate weight of the sequence based on frequency of the each base type
 def seq_weight(sequence,w_A,w_C,w_T,w_G):
     gweight=0
     for base in sequence:
@@ -25,23 +38,38 @@ def seq_weight(sequence,w_A,w_C,w_T,w_G):
         if base=='G':
             gweight+=w_G
     return gweight
+#################################END############################################
 
+
+#################################BEGIN##########################################
+#calculate weighted length of the cluster
 def calculate_wl(cluster_gw,cluster_gl,c,wln):
     wt=0
     for j in range (0,wln+1):
         wt+=cluster_gw[c][j]*cluster_gl[c][j]
     return wt
+#################################END############################################
 
+
+#################################BEGIN##########################################
+#Measure similarity between input sequence  and reference gene of the cluster
 def sim(temp_w,temp_l,lref_cl,wref_cl):
     return abs(wref_cl*lref_cl-temp_w*temp_l)
+#################################END############################################
 
 
+#################################BEGIN##########################################
+#Find the position of gene with closest gene; find the position of the sequence
+#in a cluster whose length  is closest to the average sequence length of the
+#cluster
 def closest(list, Number):
     aux = []
     for gsq in list:
         gsq_l=len(gsq)
         aux.append(abs(Number-gsq_l))
     return aux.index(min(aux))
+#################################END############################################
+
 
 
 if __name__=="__main__":
@@ -51,6 +79,7 @@ if __name__=="__main__":
         f = open(sys.argv[1],"r")
     fasta = f.readlines()
     total_input_seq=0
+#####################Start:File Format Check & Base Count#######################
     if file_format_check(fasta)==True:
         print("File contains appropriate character")
         counter ={'A':0, 'C':0,'T':0,'G':0}
@@ -70,13 +99,18 @@ if __name__=="__main__":
         #print(counter)
         N=counter['A']+counter['C']+counter['T']+counter['G']
         #print(N)
+#######################END:File Format Check & Base Count#######################
 
+
+#######################Start: Initialization, Algorithm Step-1-2 ###############
         w_A=counter['A']/N
         w_C=counter['C']/N
         w_T=counter['T']/N
         w_G=counter['G']/N
 
         print("Total Input sequence",total_input_seq)
+
+
         c=0
         n=0
         wln=0#index for seq weight and length
@@ -92,7 +126,9 @@ if __name__=="__main__":
         l_cmax=[]
         cur_size=[]
         avg_length_c=[]
-        thresh_p=0.7
+        thresh_p=0.55 # Important Parameter; Change in value result different output
+
+
         for line_number,line in enumerate(fasta):
             if line_number%3==0:
                 fname=line.strip('>')
@@ -105,8 +141,13 @@ if __name__=="__main__":
                 sequence=sequence.replace("M","C")
                 seq_n=seq_n+1
                 L_seq=len(sequence)
+
                 assigned_cluster=200
                 min_dism=math.inf
+#######################End: Initialization, Algorithm Step-1-2 #################
+
+#######################Start: Algorithm Step-3-4 ###############################
+
                 if seq_n==1:
                     ref.append(sequence)
                     cluster[c].append(sequence) #sequence cluster
@@ -117,17 +158,20 @@ if __name__=="__main__":
                     w_cmax.append(cluster_gw[c][post])
                     l_cmax.append(max(cluster_gl[c]))
                     #threshold
-                    print(w_cmax[c])
-                    print(l_cmax[c])
+                    #print(w_cmax[c])
+                    #print(l_cmax[c])
                     cluster_th.append(w_cmax[c]*l_cmax[c]*0.55)
-                    print(w_cmax[c]*l_cmax[c])
+                    #print(w_cmax[c]*l_cmax[c])
                     cur_size.append(np.count_nonzero(cluster_gl[c]))
 
                     cluster_wl.append(calculate_wl(cluster_gw,cluster_gl,c,wln))
                     total_cl_seq=len(cluster[c])
                     total_cl_seq_length=sum(cluster_gl[c])
                     avg_length_c.append(total_cl_seq_length/total_cl_seq)
+
+#######################End: Algorithm Step-3-4 #################################
                 else:
+#######################Start: Algorithm Step-5.1 ###############################
                     for cl in range(0,c+1):
                         temp_w=seq_weight(sequence,w_A,w_C,w_T,w_G)
                         temp_l=len(sequence)
@@ -137,6 +181,9 @@ if __name__=="__main__":
                         if sim_measure<cluster_th[cl] and sim_measure<min_dism:
                             min_dism=sim_measure
                             assigned_cluster=cl
+########################End: Algorithm Step-5.1 ################################
+
+########################Start: Algorithm Step-5.3 ##############################
                     if assigned_cluster==200:#new cluster create
                         c=c+1
                         wln=0########################check
@@ -154,7 +201,13 @@ if __name__=="__main__":
                         total_cl_seq=len(cluster[c])
                         total_cl_seq_length=sum(cluster_gl[c])
                         avg_length_c.append(total_cl_seq_length/total_cl_seq)
+
+########################End: Algorithm Step-5.3 ################################
+
                     else:
+
+########################Start: Algorithm Step-5.2 ##############################
+
                         cluster[assigned_cluster].append(sequence) #sequence cluster
                         temp_w=seq_weight(sequence,w_A,w_C,w_T,w_G)
                         lg=len(sequence)
@@ -172,15 +225,18 @@ if __name__=="__main__":
                         w_cmax[assigned_cluster]=cluster_gw[assigned_cluster][post]
                         l_cmax[assigned_cluster]=max(cluster_gl[assigned_cluster])
                         cluster_th[assigned_cluster]=w_cmax[assigned_cluster]*l_cmax[assigned_cluster]*0.55
-        # df=pd.DataFrame()
-        # df[0]=[thresh_p,total_input_seq,c+1]
-        # df=df.T
-        # df.columns=['Cluster Threshold','Total Input sequence','Number of Generated Cluster']
-        # #fname="Threshold"+thresh_p+".txt"
-        # df.to_csv("0.20-10.txt",index=False, sep=' ', header=True)
-        print(w_cmax)
-        print(l_cmax)
+
+########################End: Algorithm Step-5.2 ################################
+
+        df=pd.DataFrame()
+        df[0]=[thresh_p,total_input_seq,c+1]
+        df=df.T
+        df.columns=['Cluster Threshold','Total Input sequence','Number of Generated Cluster']
+        #fname="Threshold"+thresh_p+".txt"
+        df.to_csv("0.55-10.txt",index=False, sep=' ', header=True) # change filename according to threshold value and number of input sequences
+
+####################### Write output File ######################################
+        #print(w_cmax)
+        #print(l_cmax)
         for t in range(0,c+1):
             print("Cluster:",t+1,len(cluster[t]))
-
-##*****C,CN,WLN, increase + H_At calculate korte hobe for new cluster
